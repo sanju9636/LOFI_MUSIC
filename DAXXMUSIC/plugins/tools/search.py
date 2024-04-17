917ab26594a6b9b083d6030909874dde315c81a2
Sanju <141401968+sanju9636@users.noreply.github.com>
2024-04-17 22:48:30 +0530

    Update search.py

--- DAXXMUSIC/plugins/tools/search.py
+++ DAXXMUSIC/plugins/tools/search.py
@@ -123,7 +123,7 @@ async def search_(app: app, msg: Message):
         return
     except Exception as e:
         await to_del.delete()
-        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @OP_SHIVA_007)
+        await msg.reply_text(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ :\nʀᴇᴘᴏʀᴛ ᴀᴛ ɪᴛ** @OP_SHIVA_007")
         print(f"error : {e}")
         return
 
